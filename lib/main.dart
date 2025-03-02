ef5a0e45c2740cddbd99a798e84918bc9817ad16
Mohamed Soliman <ms3196@fayoum.edu.eg>
2025-03-02 23:59:23 +0200

    - set app orientation to portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:canc_app/canc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  // Set app orientation to portrait mode only
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const CancApp());
 }
