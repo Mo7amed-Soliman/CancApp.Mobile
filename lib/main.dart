5da84150aa3cb804336baddde568a617d0e441f3
Mohamed Soliman <ms3196@fayoum.edu.eg>
2025-03-04 15:23:40 +0200

    - add device_preview for responsive testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:canc_app/canc_app.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   // Set app orientation to portrait mode only
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(const CancApp());
+  // Run the app
+  runApp(
+    DevicePreview(
+      enabled: kDebugMode,
+      builder: (context) => const CancApp(),
+    ),
+  );
 }
