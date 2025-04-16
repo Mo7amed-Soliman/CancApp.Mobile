403434c447bc2d1b2fe6eb41f2fda33fb21dc317
Mohamed Soliman <ms3196@fayoum.edu.eg>
2025-04-17 01:54:50 +0200

    - create hive helper class and init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:canc_app/canc_app.dart';
 import 'package:canc_app/core/di/dependency_injection.dart';
 import 'package:canc_app/core/helpers/database/cache_helper.dart';
+import 'package:canc_app/core/helpers/database/hive_helper.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -20,6 +21,9 @@ void main() async {
 
   /// Initialize cache helper
   await getIt<CacheHelper>().init();
+
+  /// Initialize Hive
+  await HiveHelper.init();
 
   /// Run the app
   runApp(
