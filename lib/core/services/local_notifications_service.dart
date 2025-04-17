0c521f3af2ab9810787fdf51927cc4111b7b38e8
Mohamed Soliman <ms3196@fayoum.edu.eg>
2025-04-17 04:20:12 +0200

    - add flutter local notification package and setup

---
+++ lib/core/services/local_notifications_service.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  static StreamController<NotificationResponse> streamController =
+      StreamController();
+
+  static void onTap(NotificationResponse notificationResponse) {
+    streamController.add(notificationResponse);
+  }
+
+  static Future<void> init() async {
+    InitializationSettings settings = const InitializationSettings(
+      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      iOS: DarwinInitializationSettings(),
+    );
+    flutterLocalNotificationsPlugin.initialize(
+      settings,
+      onDidReceiveNotificationResponse: onTap,
+      onDidReceiveBackgroundNotificationResponse: onTap,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:canc_app/canc_app.dart';
 import 'package:canc_app/core/di/dependency_injection.dart';
 import 'package:canc_app/core/helpers/database/cache_helper.dart';
 import 'package:canc_app/core/helpers/database/hive_helper.dart';
+import 'package:canc_app/core/services/local_notifications_service.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,6 +25,9 @@ void main() async {
 
   /// Initialize Hive
   await HiveHelper.init();
+
+  /// Initialize Local Notification Service
+  await LocalNotificationService.init();
 
   /// Run the app
   runApp(
