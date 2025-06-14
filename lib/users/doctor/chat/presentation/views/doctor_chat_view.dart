384af427966f27930cbe1ec7bcee12264b00dc20
Mohamed Soliman <ms3196@fayoum.edu.eg>
2025-06-14 15:56:26 +0300

    - feat: Add doctor chat view and integrate it into bottom navigation

---
+++ lib/users/doctor/chat/presentation/views/doctor_chat_view.dart
@@ -0,0 +1,116 @@
+import 'package:canc_app/core/helpers/responsive_helpers/size_helper_extension.dart';
+import 'package:canc_app/core/theming/app_colors.dart';
+import 'package:canc_app/core/theming/app_styles.dart';
+import 'package:canc_app/core/widgets/horizontal_spacer.dart';
+import 'package:canc_app/core/widgets/vertical_spacer.dart';
+import 'package:canc_app/generated/l10n.dart';
+import 'package:canc_app/users/patient/chat/presentation/views/widgets/chats_list.dart';
+import 'package:canc_app/users/patient/chat/presentation/views/widgets/search_chats_button.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../patient/chat/presentation/views/widgets/search_field_chats.dart';
+
+class DoctorChatView extends StatelessWidget {
+  const DoctorChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        DoctorChatHeader(),
+        VerticalSpacer(8),
+        Expanded(
+          child: ChatsList(),
+        ),
+      ],
+    );
+  }
+}
+
+class DoctorChatHeader extends StatefulWidget {
+  const DoctorChatHeader({super.key});
+
+  @override
+  State<DoctorChatHeader> createState() => _DoctorChatHeaderState();
+}
+
+class _DoctorChatHeaderState extends State<DoctorChatHeader>
+    with SingleTickerProviderStateMixin {
+  bool _isSearchVisible = false;
+  final TextEditingController _searchController = TextEditingController();
+  late final AnimationController _animationController;
+  late final Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(-1.0, 0.0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.ease,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _toggleSearch() {
+    if (!mounted) return;
+
+    setState(() {
+      _isSearchVisible = !_isSearchVisible;
+      if (_isSearchVisible) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+        _searchController.clear();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.primaryColor,
+      padding: EdgeInsets.symmetric(
+        horizontal: context.setMinSize(16),
+      ),
+      height: context.screenHeight * 0.1,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          if (!_isSearchVisible)
+            Text(
+              S.of(context).cancApp,
+              style: AppTextStyle.font28RighteousPrimary(context).copyWith(
+                color: AppColors.offWhite,
+                height: 1.86,
+              ),
+            )
+          else
+            SearchFieldChats(
+              controller: _searchController,
+              slideAnimation: _slideAnimation,
+            ),
+          const HorizontalSpacer(10),
+          SearchChatsButton(
+            isSearchVisible: _isSearchVisible,
+            onTap: _toggleSearch,
+          ),
+        ],
+      ),
+    );
+  }
+}
